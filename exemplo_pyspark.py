06a35840cde8980607634ee03350fbf8ea02da45
mauro-gutierres <maurogutierres.sp@gmail.com>
2025-02-22 20:05:11 -0300

    correcao datas

--- exemplo_pyspark.py
+++ exemplo_pyspark.py
@@ -6,7 +6,7 @@ Descrição: pipeline que faz o ETL de arquivos csv gerando um arquivo parquet
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
-from pyspark.sql.types import DoubleType,StringType
+from pyspark.sql.types import *
 
 files_path='/home/mauro/Documents/exemplo_pyspark/data'
 
@@ -38,15 +38,15 @@ for index,colName in enumerate(empresasColNames):
 for index,colName in enumerate(sociosColNames):
     socios = socios.withColumnRenamed(f"_c{index}",colName)
 
-# altera o separador de casas decimais de ponto para virgula
+# altera o separador de casas decimais de virgula para ponto
 empresas = empresas.withColumn('capital_social_da_empresa',f.regexp_replace('capital_social_da_empresa',',','.'))
 
 # altera o datatype da coluna para double
 empresas = empresas.withColumn('capital_social_da_empresa',empresas['capital_social_da_empresa'].cast(DoubleType()))
 
 # altera os campos data de string para date
-estabelecimentos = estabelecimentos.withColumn("data_situacao_cadastral",f.to_date(estabelecimentos.data_situacao_cadastral.cast(StringType()),'yyyyMMdd')).withColumn("data_de_inicio_atividade",f.to_date(estabelecimentos.data_de_inicio_atividade.cast(StringType()),'yyyyMMdd')).withColumn("data_da_situacao_especial",f.to_date(estabelecimentos.data_da_situacao_especial.cast(StringType()),'yyyyMMdd'))
-socios = socios.withColumn("data_de_entrada_sociedade",f.to_date(socios.data_de_entrada_sociedade.cast(StringType()),'yyyyMMdd'))
+#estabelecimentos = estabelecimentos.withColumn("data_situacao_cadastral",f.to_date(estabelecimentos.data_situacao_cadastral.cast(StringType()),'yyyyMMdd')).withColumn("data_de_inicio_atividade",f.to_date(estabelecimentos.data_de_inicio_atividade.cast(StringType()),'yyyyMMdd')).withColumn("data_da_situacao_especial",f.to_date(estabelecimentos.data_da_situacao_especial.cast(StringType()),'yyyyMMdd'))
+#socios = socios.withColumn("data_de_entrada_sociedade",f.to_date(socios.data_de_entrada_sociedade.cast(StringType()),'yyyyMMdd'))
 
 '''
 Load - Persiste os dataframes em tabelas no postgres e gera arquivo parquet
@@ -58,8 +58,7 @@ socios.write.format("jdbc").option("url","jdbc:postgresql://localhost:5432/cadem
 
 # gera arquivo parquet, a partir de join das tabelas
 join_df = estabelecimentos.alias('est').join(empresas.alias('emp'),'cnpj_basico', how="inner").join(socios.alias('soc'), 'cnpj_basico', how='inner').select('est.cnpj_basico', 'est.cnpj_ordem','est.nome_fantasia', 'est.identificador_matriz_filial', 'est.data_de_inicio_atividade', 'est.municipio', 'emp.razao_social_nome_empresarial', 'emp.capital_social_da_empresa', 'emp.porte_da_empresa', 'soc.identificador_de_socio', 'soc.nome_do_socio_ou_razao_social', 'soc.qualificacao_do_socio')
-join_df.write.parquet(path=files_path+"/empresas_out/empresas", mode='overwrite')
-
+join_df.write.format("parquet").save(files_path+"/empresas_out/empresas", mode='overwrite')
 
 spark.stop()
 
